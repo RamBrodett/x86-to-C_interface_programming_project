508434816a6a178181e1a01918aab5aaa553e451
rmbrodett <rmbrodett@gmail.com>
2024-11-26 00:56:31 +0800

    fix(main): fix the parameter type for destination

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern imgCvtGrayIntToDouble(int height, int width, int* src2dArray, int* dest2dArray);
+extern imgCvtGrayIntToDouble(int height, int width, int* src2dArray, double* dest2dArray);
 
 int main(){
     int height, width;
